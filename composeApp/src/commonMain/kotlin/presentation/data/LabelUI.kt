e7c53ad5307733cc5eca2a7f552959b717a15100
Alexis Aneas <alexis.aneas@outlook.fr>
2024-08-12 20:05:32 +0200

    define default labels

--- composeApp/src/commonMain/kotlin/presentation/data/LabelUI.kt
+++ composeApp/src/commonMain/kotlin/presentation/data/LabelUI.kt
@@ -6,4 +6,15 @@ data class LabelUI(
     val id: Int,
     val name: String,
     val color: Color,
-)
+) {
+    companion object {
+        val defaults = listOf(
+            LabelUI(1, "Royal Blue", Color(0xFF4169E1)),
+            LabelUI(2, "Emerald Green", Color(0xFF50C878)),
+            LabelUI(3, "Golden Amber", Color(0xFFFFBF00)),
+            LabelUI(4, "Crimson Red", Color(0xFFDC143C)),
+            LabelUI(5, "Deep Purple", Color(0xFF800080)),
+            LabelUI(6, "Midnight Black", Color(0xFF191970))
+        )
+    }
+}
--- composeApp/src/commonMain/kotlin/ui/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/MainViewModel.kt
@@ -3,6 +3,7 @@ package ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import data.repository.AppPreferences
+import data.repository.LabelRepository
 import data.repository.PreferenceRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -11,9 +12,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import presentation.data.LabelUI
 
 class MainViewModel(
-    preferenceRepository: PreferenceRepository
+    preferenceRepository: PreferenceRepository,
+    labelRepository: LabelRepository
 ): ViewModel() {
 
     private var preferencesJob: Job? = null
@@ -26,6 +29,13 @@ class MainViewModel(
         preferencesJob = viewModelScope.launch(Dispatchers.IO) {
             preferenceRepository.preferences.collect { preferences ->
                 _preferences.update { preferences }
+            }
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val labels = labelRepository.getLabels()
+            if (labels.isEmpty()) {
+                labelRepository.addLabels(LabelUI.defaults)
             }
         }
     }
